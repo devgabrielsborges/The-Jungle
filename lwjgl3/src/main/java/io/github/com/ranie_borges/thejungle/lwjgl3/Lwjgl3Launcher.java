4e259773cf93cdfb140e7460488bbfbbdd7fc171
devgabrielsborges <dev.gabrielsborges@proton.me>
2025-02-28 20:35:45 +0000

    build: fixed package name

--- lwjgl3/src/main/java/io/github/com/ranie_borges/thejungle/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/com/ranie_borges/thejungle/lwjgl3/Lwjgl3Launcher.java
@@ -1,8 +1,8 @@
-package io.github.com.ranie_borges.theforestrpg.lwjgl3;
+package io.github.com.ranie_borges.thejungle.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.github.com.ranie_borges.theforestrpg.Main;
+import io.github.com.ranie_borges.thejungle.Main;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
