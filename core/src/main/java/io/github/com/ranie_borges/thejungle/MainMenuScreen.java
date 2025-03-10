a016442cc6a7547e85a69323508f357cfb4f95b2
TheDotOs <rcamposlins@gmail.com>
2025-03-10 17:48:10 -0300

    Deleta o GameScreen

--- core/src/main/java/io/github/com/ranie_borges/thejungle/MainMenuScreen.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/MainMenuScreen.java
@@ -4,11 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
 public class MainMenuScreen implements Screen {
 
@@ -52,7 +55,7 @@ public class MainMenuScreen implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.app.log("MainMenuScreen", "Play button clicked!");
-                game.setScreen(new GameScreen(game));
+                //game.setScreen(new GameScreen(game)); //Add a tela do jogo
             }
         });
 
