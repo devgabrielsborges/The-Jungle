93d0363fe36c300686d891ba037f56d18f09f5a1
devgabrielsborges <dev.gabrielsborges@proton.me>
2025-03-11 12:12:32 +0000

    feat: add ARMOR type to ItemType enum

--- core/src/main/java/io/github/com/ranie_borges/thejungle/model/enums/ItemType.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/model/enums/ItemType.java
@@ -1,5 +1,10 @@
 package io.github.com.ranie_borges.thejungle.model.enums;
 
 public enum ItemType {
-    MATERIAL, MEDICINE, TOOL, WEAPON, DRINKABLE
+    ARMOR,
+    MATERIAL,
+    MEDICINE,
+    TOOL,
+    WEAPON,
+    DRINKABLE
 }
