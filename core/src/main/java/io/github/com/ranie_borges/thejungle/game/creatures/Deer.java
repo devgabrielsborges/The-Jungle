5439827048d49a1a7238cf6f410252685a33da62
devgabrielsborges <devgabrielsborges@proton.me>
2025-03-18 21:02:14 +0000

    refactor: simplify attack method in Bird, Bunny, Cannibal, Deer, Fish, and Lizard classes

--- core/src/main/java/io/github/com/ranie_borges/thejungle/game/creatures/Deer.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/game/creatures/Deer.java
@@ -24,47 +24,8 @@ public class Deer extends Creature {
         super(name, description, probability, lifeRatio, damage, climeSpawn, drops, sprites);
     }
 
-    /**
-     * @param attackDamage The amount of damage to inflict
-     * @param npc          The non-player character target
-     * @return
-     */
     @Override
-    public boolean attack(double attackDamage, NonPlayerCharacter npc) {
-        return false;
-    }
-
-    /**
-     * @param hasTraitLucky Whether the character has the lucky trait
-     * @return
-     */
-    @Override
-    public boolean avoidFight(boolean hasTraitLucky) {
-        return false;
-    }
-
-    /**
-     * @param hasItemNear     Whether an item is available nearby
-     * @param isInventoryFull Whether the character's inventory is full
-     */
-    @Override
-    public void collectItem(boolean hasItemNear, boolean isInventoryFull) {
-
-    }
-
-    /**
-     * @param hasDrinkableItem Whether the character has a drinkable item
-     */
-    @Override
-    public void drink(boolean hasDrinkableItem) {
-
-    }
-
-    /**
-     * @param item The item to use
-     */
-    @Override
-    public void useItem(Item item) {
+    public void attack() {
 
     }
 }
