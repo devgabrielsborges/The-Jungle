81cf97953963e0a96ba5f642b1ea6941f30e3875
devgabrielsborges <dev.gabrielsborges@proton.me>
2025-03-20 00:44:21 +0000

    feat: add Climate, Injury, and Mob event classes with basic functionality

---
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/game/events/Injury.java
@@ -0,0 +1,48 @@
+package io.github.com.ranie_borges.thejungle.game.events;
+
+import io.github.com.ranie_borges.thejungle.model.Character;
+import io.github.com.ranie_borges.thejungle.model.Event;
+
+public class Injury extends Event {
+    private float impact;
+    private float healRate;
+    private boolean isHealable;
+
+    protected Injury(String name, String description, float probability) {
+        super(name, description, probability);
+    }
+
+    public float getImpact() {
+        return impact;
+    }
+
+    public void setImpact(float impact) {
+        this.impact = impact;
+    }
+
+    public float getHealRate() {
+        return healRate;
+    }
+
+    public void setHealRate(float healRate) {
+        this.healRate = healRate;
+    }
+
+    public boolean isHealable() {
+        return isHealable;
+    }
+
+    public void setHealable(boolean healable) {
+        isHealable = healable;
+    }
+
+    /**
+     * @param character
+     * @param <T>
+     */
+    @Override
+    public <T extends Character> void execute(T character) {
+        //TODO to implement
+    }
+
+}
---
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/game/events/Mob.java
@@ -0,0 +1,69 @@
+package io.github.com.ranie_borges.thejungle.game.events;
+
+import io.github.com.ranie_borges.thejungle.model.Character;
+import io.github.com.ranie_borges.thejungle.model.Creature;
+import io.github.com.ranie_borges.thejungle.model.Event;
+
+public class Mob<T extends Creature> extends Event {
+    private T creature;
+    private int dangerLevel;
+    private boolean isHostile;
+    private boolean isAvoidable;
+
+    public T getCreature() { return creature; }
+
+    public void setCreature(T creature) {
+        this.creature = creature;
+    }
+
+    public int getDangerLevel() {
+        return dangerLevel;
+    }
+
+    public void setDangerLevel(int dangerLevel) {
+        this.dangerLevel = dangerLevel;
+    }
+
+    public boolean isHostile() {
+        return isHostile;
+    }
+
+    public void setHostile(boolean hostile) {
+        isHostile = hostile;
+    }
+
+    public boolean isAvoidable() {
+        return isAvoidable;
+    }
+
+    public void setAvoidable(boolean avoidable) {
+        isAvoidable = avoidable;
+    }
+
+    protected Mob(
+        String name,
+        String description,
+        float probability,
+        T creature,
+        int dangerLevel,
+        boolean isAvoidable,
+        boolean isHostile
+    ) {
+        super(name, description, probability);
+        setCreature(creature);
+        setDangerLevel(dangerLevel);
+        setAvoidable(isAvoidable);
+        setHostile(isHostile);
+
+    }
+
+    /**
+     * @param character
+     * @param <U>
+     */
+    @Override
+    public <U extends Character> void execute(U character) {
+        // TODO to implement
+    }
+
+}
