0c7b59542f47306d48fbf61fe8a7a46eb9ac16c1
devgabrielsborges <dev.gabrielsborges@proton.me>
2025-03-24 10:43:08 +0000

    feat: add Main class to initialize game and set up main menu screen

---
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/core/Main.java
@@ -0,0 +1,28 @@
+package io.github.com.ranie_borges.thejungle.core;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.github.com.ranie_borges.thejungle.view.MainMenuScreen;
+
+public class Main extends Game {
+    public SpriteBatch batch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        // Define a tela inicial do jogo: o menu principal
+        setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        // Delegar o render para a tela atual
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        super.dispose();
+    }
+}
