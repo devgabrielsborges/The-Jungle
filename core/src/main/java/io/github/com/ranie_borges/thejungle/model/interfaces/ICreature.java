ac05644315b3d1e753cda1462a85f31c87521dd7
devgabrielsborges <dev.gabrielsborges@proton.me>
2025-03-24 10:43:43 +0000

    refactor: deleted and moved source files

--- core/src/main/java/io/github/com/ranie_borges/thejungle/model/interfaces/ICreature.java
+++
@@ -1,5 +0,0 @@
-package io.github.com.ranie_borges.thejungle.model.interfaces;
-
-public interface ICreature {
-    void attack();   // TODO implement default attack method
-}
--- core/src/main/java/io/github/com/ranie_borges/thejungle/model/interfaces/IEvent.java
+++
@@ -1,7 +0,0 @@
-package io.github.com.ranie_borges.thejungle.model.interfaces;
-
-import io.github.com.ranie_borges.thejungle.model.Character;
-
-public interface IEvent {
-    <T extends Character> void execute(T character);
-}
