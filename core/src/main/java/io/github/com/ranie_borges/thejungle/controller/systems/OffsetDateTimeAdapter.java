89a0560477955366d820d47ece7d51385cf57601
devgabrielsborges <dev.gabrielsborges@proton.me>
2025-03-26 14:18:57 +0000

    feat: add OffsetDateTimeAdapter for JSON serialization and deserialization of OffsetDateTime

---
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/controller/systems/OffsetDateTimeAdapter.java
@@ -0,0 +1,21 @@
+package io.github.com.ranie_borges.thejungle.controller.systems;
+
+import com.google.gson.*;
+import java.lang.reflect.Type;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class OffsetDateTimeAdapter implements JsonSerializer<OffsetDateTime>, JsonDeserializer<OffsetDateTime> {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
+    @Override
+    public JsonElement serialize(OffsetDateTime src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(formatter.format(src));
+    }
+
+    @Override
+    public OffsetDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+        throws JsonParseException {
+        return OffsetDateTime.parse(json.getAsString(), formatter);
+    }
+}
