85fabd7702b418b28e7591b1a0ae21dee819ee1c
devgabrielsborges <dev.gabrielsborges@proton.me>
2025-04-01 14:59:51 +0000

    feat: enhance NonPlayerCharacter class with attributes and methods for inventory management and interaction

--- core/src/main/java/io/github/com/ranie_borges/thejungle/model/entity/interfaces/INonPlayerCharacter.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/model/entity/interfaces/INonPlayerCharacter.java
@@ -1,5 +1,26 @@
 package io.github.com.ranie_borges.thejungle.model.entity.interfaces;
 
+import io.github.com.ranie_borges.thejungle.model.entity.Character;
+
 public interface INonPlayerCharacter {
-    //TODO
+    /**
+     * Attacks a character with the specified damage.
+     *
+     * @param attackDamage The amount of damage to inflict
+     * @param character The character to attack
+     * @return true if attack was successful, false otherwise
+     */
+    boolean attack(double attackDamage, Character<?> character);
+
+    /**
+     * Interacts with a character
+     *
+     * @param character The character to interact with
+     */
+    void interact(Character<?> character);
+
+    /**
+     * Makes the NPC roam around
+     */
+    void roam();
 }
