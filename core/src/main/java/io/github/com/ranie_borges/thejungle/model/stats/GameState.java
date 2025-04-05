01810a2d67ae59233aab9b68d1f0006d4a9477f6
devgabrielsborges <devgabrielsborges@proton.me>
2025-04-05 11:48:04 -0300

    refactor: GameState to use generics and integrate AmbientController

--- core/src/main/java/io/github/com/ranie_borges/thejungle/model/stats/GameState.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/model/stats/GameState.java
@@ -1,38 +1,43 @@
 package io.github.com.ranie_borges.thejungle.model.stats;
 
+import io.github.com.ranie_borges.thejungle.controller.AmbientController;
+import io.github.com.ranie_borges.thejungle.controller.EventController;
 import io.github.com.ranie_borges.thejungle.model.entity.Character;
-import io.github.com.ranie_borges.thejungle.model.world.Ambient;
 import io.github.com.ranie_borges.thejungle.model.events.Event;
+import io.github.com.ranie_borges.thejungle.model.world.Ambient;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GameState {
-    private Character playerCharacter;
-    private Ambient currentAmbient;
+public class GameState<C extends Character<?>, A extends Ambient> {
+    private C playerCharacter;
+    private A currentAmbient;
     private List<Event> activeEvents;
     private int daysSurvived;
     private OffsetDateTime offsetDateTime;
+    private EventController<C> eventController;
+    private AmbientController<A> ambientController;
 
     public GameState() {
         this.activeEvents = new ArrayList<>();
         this.offsetDateTime = OffsetDateTime.now();
+        this.eventController = new EventController<>(this);
     }
 
-    public Character getPlayerCharacter() {
+    public C getPlayerCharacter() {
         return playerCharacter;
     }
 
-    public void setPlayerCharacter(Character playerCharacter) {
+    public void setPlayerCharacter(C playerCharacter) {
         this.playerCharacter = playerCharacter;
     }
 
-    public Ambient getCurrentAmbient() {
+    public A getCurrentAmbient() {
         return currentAmbient;
     }
 
-    public void setCurrentAmbient(Ambient currentAmbient) {
+    public void setCurrentAmbient(A currentAmbient) {
         this.currentAmbient = currentAmbient;
     }
 
@@ -60,23 +65,19 @@ public class GameState {
         this.offsetDateTime = offsetDateTime;
     }
 
-    private EventController eventController;
-
-    public GameState() {
-        this.activeEvents = new ArrayList<>();
-        this.offsetDateTime = OffsetDateTime.now();
-        this.eventController = new EventController(this);
-    }
-
-    public EventController getEventController() {
+    public EventController<C> getEventController() {
         return eventController;
     }
 
-    public void setEventController(EventController eventController) {
+    public void setEventController(EventController<C> eventController) {
         this.eventController = eventController;
     }
 
-    public AmbientController getAmbientController() {
+    public AmbientController<A> getAmbientController() {
         return ambientController;
+    }
+
+    public void setAmbientController(AmbientController<A> ambientController) {
+        this.ambientController = ambientController;
     }
 }
