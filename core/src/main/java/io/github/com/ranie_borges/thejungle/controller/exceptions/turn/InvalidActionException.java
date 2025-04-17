848f1752c5db9e8a12129815adfe2a29726fba60
Gabriel Borges <161774213+devgabrielsborges@users.noreply.github.com>
2025-04-16 23:04:47 -0300

    feat: add TurnControllerException and InvalidActionException for turn error handling

---
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/controller/exceptions/turn/InvalidActionException.java
@@ -0,0 +1,7 @@
+package io.github.com.ranie_borges.thejungle.controller.exceptions.turn;
+
+public class InvalidActionException extends TurnControllerException {
+    public InvalidActionException(String message) {
+        super(message);
+    }
+}
