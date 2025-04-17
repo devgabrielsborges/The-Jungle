748d77df194e8633a5f0f347dc47bd3a89ab3a40
Gabriel Borges <161774213+devgabrielsborges@users.noreply.github.com>
2025-04-16 23:02:23 -0300

    feat: add EventControllerException and InvalidEventException classes for event error handling

---
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/controller/exceptions/event/InvalidEventException.java
@@ -0,0 +1,7 @@
+package io.github.com.ranie_borges.thejungle.controller.exceptions.event;
+
+public class InvalidEventException extends EventControllerException {
+    public InvalidEventException(String message) {
+        super(message);
+    }
+}
