0f79f32723f6484ffc72a1d20098b0162bd06826
TheDotOs <rcamposlins@gmail.com>
2025-04-23 11:10:03 -0300

    feat: update Cave and Ruins classes with specific descriptions and resources

--- core/src/main/java/io/github/com/ranie_borges/thejungle/model/world/ambients/Ruins.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/model/world/ambients/Ruins.java
@@ -1,13 +1,25 @@
 package io.github.com.ranie_borges.thejungle.model.world.ambients;
 
+import io.github.com.ranie_borges.thejungle.model.entity.itens.Drinkable;
+import io.github.com.ranie_borges.thejungle.model.entity.itens.Material;
 import io.github.com.ranie_borges.thejungle.model.world.Ambient;
 import io.github.com.ranie_borges.thejungle.model.enums.AmbientAttribute;
 
 import java.util.Set;
+
+import static io.github.com.ranie_borges.thejungle.model.enums.AmbientAttribute.DRY_CLIMATE;
+import static io.github.com.ranie_borges.thejungle.model.enums.Clime.DESERT;
+
 public class Ruins extends Ambient {
 
     protected Ruins(String name, String description, float difficult, Set<AmbientAttribute> attributes) {
-        super(name, description, difficult, attributes);
+        super("Ruins", "Ancient stone structures overtaken by time, with crumbling walls and hidden passages. Artifacts of a forgotten civilization may be found here.", 2.5f, Set.of(DRY_CLIMATE));
+        setClimes(Set.of(DESERT));
+        setResources(Set.of(
+            new Drinkable("Stagnant Water", 0.2f, 0.3f)
+        ));
+
+
     }
 
     /**
