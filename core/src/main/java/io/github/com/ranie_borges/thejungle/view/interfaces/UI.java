fa4c5e1cc9b9bc4e5c6f5279d40ea6305440a502
devgabrielsborges <161774213+devgabrielsborges@users.noreply.github.com>
2025-05-19 20:41:33 -0300

    refactor: create UI interface with tile and map constants

---
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/view/interfaces/UI.java
@@ -0,0 +1,14 @@
+package io.github.com.ranie_borges.thejungle.view.interfaces;
+
+public interface UI {
+    static final int TILE_SIZE = 32;
+    static final int MAP_WIDTH = 30;
+    static final int MAP_HEIGHT = 20;
+    static final int SIDEBAR_WIDTH = 300;
+
+    static final int TILE_GRASS = 0;
+    static final int TILE_WALL = 1;
+    static final int TILE_DOOR = 2;
+    static final int TILE_CAVE = 3;
+    static final int TILE_WATER = 4;
+}
