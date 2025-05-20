f08dfdc2cdd7c19c60297c25f524d6205eefdf40
devgabrielsborges <161774213+devgabrielsborges@users.noreply.github.com>
2025-05-20 14:58:12 -0300

    refactor: remove redundant `static final` modifiers in UI interface constants

--- core/src/main/java/io/github/com/ranie_borges/thejungle/view/interfaces/UI.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/view/interfaces/UI.java
@@ -3,14 +3,14 @@ package io.github.com.ranie_borges.thejungle.view.interfaces;
 import com.badlogic.gdx.Gdx;
 
 public interface UI {
-    static final int TILE_SIZE = 32;
-    static final int MAP_WIDTH = Gdx.graphics.getWidth() / TILE_SIZE - 20;
-    static final int MAP_HEIGHT = Gdx.graphics.getHeight() / TILE_SIZE;
-    static final int SIDEBAR_WIDTH = 300;
+    int TILE_SIZE = 32;
+    int MAP_WIDTH = Gdx.graphics.getWidth() / TILE_SIZE - 20;
+    int MAP_HEIGHT = Gdx.graphics.getHeight() / TILE_SIZE;
+    int SIDEBAR_WIDTH = 300;
 
-    static final int TILE_GRASS = 0;
-    static final int TILE_WALL = 1;
-    static final int TILE_DOOR = 2;
-    static final int TILE_CAVE = 3;
-    static final int TILE_WATER = 4;
+    int TILE_GRASS = 0;
+    int TILE_WALL = 1;
+    int TILE_DOOR = 2;
+    int TILE_CAVE = 3;
+    int TILE_WATER = 4;
 }
