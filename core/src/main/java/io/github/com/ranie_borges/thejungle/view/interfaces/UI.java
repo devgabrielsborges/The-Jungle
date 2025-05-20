a605946c758677052a2b5b42427b611c2ade8d91
devgabrielsborges <161774213+devgabrielsborges@users.noreply.github.com>
2025-05-19 23:18:10 -0300

    refactor: dynamic map dimensions based on screen size in UI interface

--- core/src/main/java/io/github/com/ranie_borges/thejungle/view/interfaces/UI.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/view/interfaces/UI.java
@@ -1,9 +1,11 @@
 package io.github.com.ranie_borges.thejungle.view.interfaces;
 
+import com.badlogic.gdx.Gdx;
+
 public interface UI {
     static final int TILE_SIZE = 32;
-    static final int MAP_WIDTH = 30;
-    static final int MAP_HEIGHT = 20;
+    static final int MAP_WIDTH = Gdx.graphics.getWidth() / TILE_SIZE - 20;
+    static final int MAP_HEIGHT = Gdx.graphics.getHeight() / TILE_SIZE;
     static final int SIDEBAR_WIDTH = 300;
 
     static final int TILE_GRASS = 0;
