9cacc0d9559c2ba9227784bcfc7467655e0062ad
TheDotOs <rcamposlins@gmail.com>
2025-05-23 19:26:06 -0300

    feat: enhance Fish class with spawning logic and improve item drop mechanics

--- core/src/main/java/io/github/com/ranie_borges/thejungle/model/entity/creatures/Fish.java
+++ core/src/main/java/io/github/com/ranie_borges/thejungle/model/entity/creatures/Fish.java
@@ -3,64 +3,85 @@ package io.github.com.ranie_borges.thejungle.model.entity.creatures;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
-import io.github.com.ranie_borges.thejungle.model.enums.Clime;
+import com.badlogic.gdx.math.Vector2; // <-- Import adicionado
 import io.github.com.ranie_borges.thejungle.model.entity.Creature;
 import io.github.com.ranie_borges.thejungle.model.entity.Item;
+import io.github.com.ranie_borges.thejungle.model.enums.Clime;
 import io.github.com.ranie_borges.thejungle.model.world.Ambient;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Fish extends Creature {
-    private Vector2 position = new Vector2();
+
+    private final Vector2 position = new Vector2();
+    private Ambient ambient;
+
+
+    protected Fish(
+        String name,
+        String description,
+        float probability,
+        float lifeRatio,
+        float damage,
+        Clime climeSpawn,
+        Set<Item> drops,
+        Map<String, Sprite> sprites
+    ) {
+        super(name, description, probability, lifeRatio, damage, climeSpawn, drops, sprites);
+    }
+    private boolean canSpawnFishInCurrentAmbient() {
+        String ambientName = ambient.getName();
+        return ambientName.equals("LakeRiver");
+    }
+    public static boolean canSpawnIn(Ambient ambient) {
+        String name = ambient.getName().toLowerCase();
+        return name.contains("lake") ;
+    }
+
 
     public Fish() {
         super(
-            "Peixe",
-            "Um peixe aquático que habita a água",
+            "Fish",
+            "Just a fish",
+            0.4f,
             0.5f,
-            0.5f,
-            1.0f,
-            Clime.FOREST, // Ajuste se necessário
+            3.0f,
+            Clime.LAKERIVER,
             createDrops(),
             createSprites()
         );
     }
 
-    @Override
-    public void attack() {
-        System.out.println(getName() + " está atacando!");
-    }
-
-    // Atualizado para retornar um drop com o item "Fish Meat"
-    private static Set<Item> createDrops() {
-        Set<Item> drops = new HashSet<>();
-        drops.add(new Item("RawFish", 10, 15) {
-            @Override
-            public void useItem() {
-                // Comportamento ao usar o item (restauração de vida ou fome)
-            }
-            @Override
-            public void dropItem() {
-                // Comportamento ao dropar o item
-            }
-        });
-        return drops;
-    }
-
-    public static boolean canSpawnIn(Ambient ambient) {
-        return ambient.getName().toLowerCase().contains("lake");
-    }
-
-    public static Map<String, Sprite> createSprites() {
+    private static Map<String, Sprite> createSprites() {
         Map<String, Sprite> sprites = new HashMap<>();
         sprites.put("idle", new Sprite(new Texture(Gdx.files.internal("sprites/criaturas/fish.png"))));
         return sprites;
     }
 
-    public Vector2 getPosition() {
+    private static Set<Item> createDrops() {
+        Set<Item> drops = new HashSet<>();
+        drops.add(new Item("Raw Fish", 15, 25) {
+            @Override
+            public void useItem() {
+                // comportamento ao usar
+            }
+
+            @Override
+            public void dropItem() {
+                // comportamento ao dropar
+            }
+        });
+        return drops;
+    }
+
+    public Vector2 getPosition() { // <-- Adicionado
         return position;
+    }
+
+    @Override
+    public void attack() {
     }
 }
